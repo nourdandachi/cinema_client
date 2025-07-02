85392c2719028df7c8912f818acc88a1b63d2df8
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-07-02 04:19:02 +0300

    Tried to make add showtime

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
         ) {
         alert("Please fill in all fields.");
         return;
-        }
+    }
 
 
     const movieData = {
